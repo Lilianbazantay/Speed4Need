89286d373eccdcb7df192dbc307b9de356c1c292
Valentin22r <valentin.roquejofre@epitech.eu>
2026-02-07 00:37:01 +0100

    Feat: Add pause menu

--- scenes/Player/pause_menu.gd
+++ scenes/Player/pause_menu.gd
@@ -12,3 +12,6 @@ func _input(_event: InputEvent) -> void:
 			hide()
 			is_hide = true
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainMenu/MainMenu.tscn")
