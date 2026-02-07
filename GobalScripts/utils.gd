6ca7551770a2dc6256968c1639794da300ccacbb
ElectronicIV <louis.persin@epitech.eu>
2026-02-07 15:53:34 +0100

    feat(utils): add min load time param to scene loader

--- GobalScripts/utils.gd
+++ GobalScripts/utils.gd
@@ -1,6 +1,7 @@
 extends Node
 
-func load_screen_to_scene(target: String) -> void:
+func load_screen_to_scene(target: String, min_load_time: float = 0.5) -> void:
 	var loading_screen = preload("res://scenes/loading/loading_screen.tscn").instantiate()
 	loading_screen.scene_path = target
+	loading_screen.min_load_time = min_load_time
 	get_tree().current_scene.add_child(loading_screen)
