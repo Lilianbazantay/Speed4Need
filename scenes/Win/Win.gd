f67669703827c4aea88edc540de535d82327f8e8
ElectronicIV <louis.persin@epitech.eu>
2026-02-07 22:38:58 +0100

    fix(Win): defer scene change

--- scenes/Win/Win.gd
+++ scenes/Win/Win.gd
@@ -1,4 +1,4 @@
 extends Node3D
 
 func _on_area_3d_area_entered(_area: Area3D) -> void:
-	get_tree().change_scene_to_file("res://scenes/Win/WinScene.tscn")
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/Win/WinScene.tscn")
