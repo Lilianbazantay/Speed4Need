3a0bc4f5856c87ac27d7821efca7b85acca45afb
ElectronicIV <louis.persin@epitech.eu>
2026-02-07 22:46:00 +0100

    fix(timer): reset timer properly

--- scenes/Player/timer.gd
+++ scenes/Player/timer.gd
@@ -1,12 +1,17 @@
 extends Control
 
+var start: int = 0
 var time: int = 0
 
 func _ready() -> void:
+
+	start = Time.get_ticks_msec()
+	print("player created at %.2f" % start)
 	if (!PlayerSettings.timerEnabled):
 		$Time.hide();
 
+
 func _process(_delta: float) -> void:
 	time = Time.get_ticks_msec()
-	$Time.text = "Time : %.2fs" % (time / 1000.0)
-	PlayerRecord.timer = time
+	$Time.text = "Time : %.2fs" % ((time - start) / 1000.0)
+	PlayerRecord.timer = time - start
